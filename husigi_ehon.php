199683a6da993fc054dd16c7870c01ba40b214dd
fujimaro17006 <04fujiisazabys@gmail.com>
2018-11-05 18:11:06 +0900

    html→php
    拡張子の変更

---
+++ husigi_ehon.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <title>ふしぎなえほん一覧</title>
+</head>
+
+<body>
+    <h1>ふしぎなえほん</h1>
+
+
+</body>
+</html>
